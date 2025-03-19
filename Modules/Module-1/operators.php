0101da2753e6850f33a5b40d6e3561768f562d77
Dev-Gabriel-Martins <gabrielgmartins100@gmail.com>
2025-03-19 11:39:26 -0300

    operators

---
+++ Modules/Module-1/operators.php
@@ -0,0 +1,12 @@
+<?php
+$valor1 = 100;
+$valor2 = '100';
+
+// == compara apenas o contéudo
+print $valor1 == $valor2 ? "true" : "false";
+
+print "</br>";
+
+// === compara contéudo e tipo
+print $valor1 === $valor2 ? "true" : "false";
+
