3d6460e7d62a118099cf5497e5b2c6479938efee
Dev-Gabriel-Martins <gabrielgmartins100@gmail.com>
2025-03-21 10:09:14 -0300

    file

--- Modules/Module-1/handler_file.php
+++ Modules/Module-1/handler_file.php
@@ -14,5 +14,18 @@ while(!feof($handler))
 print fgets($handler, 4096);
 print '<br>';
 }
-fclone($handler);
 
+// Usando o file put e get constents
+file_put_contents('/tmp/testes/teste2.txt', "a \n b \n c");
+
+print file_get_contents('/tmp/testes/teste2.txt');
+
+//File, converte arquivo em array onde cada linha é um índice
+$file = file('/tmp/testes/teste.txt');
+foreach ($file as $row)
+{
+    print '<br>' . $row;
+}
+
+
+fclose($handler);
